73d3e33463c99d1d5557868e922525f377fcba41
Isaack Njama <isaacknjama@proton.me>
2024-06-15 09:12:25 +0300

    style: add theming

---
+++ frontend/src/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Mulish, Arial, sans-serif',
+  },
+  palette: {
+    background: {
+      default: '#4AAO88'
+    }
+  }
+});
+
+export default theme;
